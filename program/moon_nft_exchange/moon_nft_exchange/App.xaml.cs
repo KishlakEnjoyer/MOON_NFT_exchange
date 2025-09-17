26bf5b56b090359eed86c601afefc27456c252f4
KishlakEnjoyer <erik.fayzullin.2007@mail.ru>
2025-09-17 10:26:07 +0500

    первичные данные загрузил

---
+++ program/moon_nft_exchange/moon_nft_exchange/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace moon_nft_exchange
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
